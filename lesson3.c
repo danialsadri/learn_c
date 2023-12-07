3c03a69e0b2cc2c15a8379f93a21783a400eeff4
danial sadri <danielsadri01@gmail.com>
2023-12-07 17:57:15 +0330

    add 2, 3, 4

---
+++ lesson3.c
@@ -0,0 +1,108 @@
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// ================================================================
+// if - else - else if 
+// > >= < <= == !=  
+// switch (expression) case break default
+// for (initializationStatement; testExpression; updateStatement)
+// while loop
+// do while loop
+// break
+// continue
+// 0 = false
+// 1 = true
+// i++
+// i--
+// ++i
+// --i
+// ================================================================
+// int main(){
+//     int option;
+//     printf("Welcome \n [1] start \n [2] Save \n [3] Exit \n");
+//     printf("Please enter your choise: ");
+//     scanf("%d", &option);
+//     if (option == 1){
+//         printf("start");
+//     }
+//     else if (option == 2){
+//         printf("save");
+//     }
+//     else if (option >= 100 || option != 4 && option < 2){
+//         printf("Exit");
+//     }
+// }
+// ================================================================
+// int main(){
+//     int option;
+//     printf("Welcome \n [1] start \n [2] Save \n [3] Exit \n");
+//     printf("Please enter your choise: ");
+//     scanf("%d", &option);
+//     switch (option){
+//         case 1:
+//             printf("Start");
+//             break;
+        
+//         case 2:
+//             printf("Save");
+//             break;
+        
+//         case 3:
+//             printf("Exit");
+//             break;
+        
+//         default:
+//             printf("Goodbye");
+//             break;
+//     }
+// }
+// ================================================================
+// int main(){
+//     for (int i = 0; i < 10; i++){
+//         printf("%d\n", i);
+//     }
+// }
+// ================================================================
+// int main(){
+//     for (int i = 0; 0; i++){
+//         printf("%d\n", i);
+//     }
+// }
+// ================================================================
+// int main(){
+//     int i = 0;
+//     while (i <= 10){
+//         printf("%d\n", i);
+//         i++;
+//     }
+// }
+// ================================================================
+// int main(){
+//     int i = 0;
+//     do{
+//         printf("hello world\n");
+//         i++;
+//     }
+//     while (i <= 10);
+// }
+// ================================================================
+// int main(){
+//     for(int i=0; i<=10; i++){
+//         printf("%d\n", i);
+//         if(i == 5){
+//             printf("Goodbye!!!");
+//             // break;
+//             continue;
+//         }
+//     }
+// }
+// ================================================================
+// int main(){
+//     for(int i=1; i<10; i++){
+//         for(int j=0; j<i; j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
+// ================================================================
---
+++ lesson4.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+// ===================================================================================
+// Function
+// function prototype
+// function call
+// fnction definition
+// return statement
+// arguments
+// c recursion
+// global variable
+// local variable
+// static variable
+// factoral
+// is_prime
+// display all prime numbers between two intervals
+// find G.C.D using recursion
+// calculate the power of a number using recursion
+// <return type> <name>(arguments){ }
+// ===================================================================================
+// Function
+// int main(){
+//     printf("hello");
+//     return 0;
+// }
+// ===================================================================================
+// void test(){ // function prototype
+//     printf("hello");
+// }
+
+// int main(){
+//     test(); // function call
+// }
+// ===================================================================================
+// int test(){
+//     int a = 10;
+//     return a;
+// }
+
+// int main(){
+//     int myNumber = test();
+//     printf("%d", myNumber);
+// }
+// ===================================================================================
+// int MyFunction(int x){
+//     return x*x;
+// }
+
+// int main(){
+//     printf("%d", MyFunction(4));
+// }
+// ===================================================================================
+// void MyFunction(char name[]){
+//     printf("Hello %s", name);
+// }
+
+// int main(){
+//     MyFunction("ahmad");
+// }
+// ===================================================================================
